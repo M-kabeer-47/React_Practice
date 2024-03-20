98d83de5432523163d784931af917d87a5012295
unknown <Muhammadkabeer2003@gmail.com>
2024-03-20 16:08:29 +0500

    Props

---
+++ Props/src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+export default function App(){
+  return(
+    <>
+    <Card name="Beyonce" img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" email ="b@beyonce.com" tel ="+123 456 789"/>
+    <Card name="Jack Bauer" img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png" email ="jack@nowhere.com" tel ="+987 654 321"/>
+    <Card name="Chuck Norris" img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png" email ="gmail@chucknorris.com" tel ="+918 372 574"/>
+
+    </>
+  )
+}
---
+++ Props/src/Card.jsx
@@ -0,0 +1,13 @@
+export default function Card(props) {
+  return (
+    <>
+      <h2>{props.name}</h2>
+      <img
+        src={props.img}
+        alt="avatar_img"
+      />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </>
+  );
+}
